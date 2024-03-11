63ed900fbfaf28301ef366cf1a8326043e41ca7a
meikun1 <150388356+meikun1@users.noreply.github.com>
2024-03-11 19:50:36 +0300

    fixed(mail)

--- app/src/main/java/com/example/shoplist/RecoverActivity.kt
+++ app/src/main/java/com/example/shoplist/RecoverActivity.kt
@@ -2,10 +2,27 @@ package com.example.shoplist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class RecoverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recover)
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    fun sendEmail(view: View) {
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                MailSender.main(arrayOf())
+            } catch (e: Exception) {
+                e.printStackTrace()
+                // Здесь обрабатывайте исключение, например, выводите сообщение об ошибке
+            }
+        }
     }
 }
